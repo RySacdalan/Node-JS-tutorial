c6cee57741ece0b57b3f03e6ead1aeb77cf4342c
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-02-10 19:30:25 +0800

    Node js basic routing.

--- server.js
+++ server.js
@@ -8,7 +8,22 @@ const server = http.createServer((req, res) => {
 
   //Setting up the header content type
   res.setHeader("Content-type", "text/html");
-  fs.readFile("./views/index.html", (err, data) => {
+
+  //Basic routes
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      break;
+    case "/about":
+      path += "about.html";
+      break;
+    default:
+      path += "404.html";
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
