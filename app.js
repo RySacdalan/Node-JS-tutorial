53e5290cfc1bf01af219d87e52624e72834afe92
developer@trashcash.ph <sacdalanryanchristian6404@gmail.com>
2022-02-12 14:57:06 +0800

    Install ejs view engine.

--- app.js
+++ app.js
@@ -3,17 +3,24 @@ const express = require("express");
 //creating express app
 const app = express();
 
+//register view engines
+app.set("view engine", "ejs");
+
 //create listener for request
 app.listen(3000);
 
 app.get("/", (req, res) => {
   //res.send("<p>This is from app.js</p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
   //res.send("<p>This is from about page</p>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 //redirects
@@ -23,5 +30,5 @@ app.get("/about-us", (req, res) => {
 
 //404 redirects
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
